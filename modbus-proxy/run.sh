9fbf78ea20b0eac0faf70622f2051592180f3bcb
edomi-fan <126984260+edomi-fan@users.noreply.github.com>
2025-05-18 14:04:01 +0200

    Update run.sh
    Added UnitID re-mapping

--- modbus-proxy/run.sh
+++ modbus-proxy/run.sh
@@ -6,6 +6,7 @@ set +x
 CONFIG_HOST=$(bashio::config 'upstreamhost')
 CONFIG_PORT=$(bashio::config 'upstreamport')
 CONFIG_LISTENPORT=$(bashio::config 'listenport')
+CONFIG_UNITIDREMAPPING=$(bahio::config 'unitid_remapping'
 CONFIG_TIMEOUT=$(bashio::config 'timeout')
 CONFIG_CONNECTIONTIME=$(bashio::config 'connection_time')
 CONFIG_LOGLEVEL=$(bashio::config 'loglevel')
@@ -14,6 +15,7 @@ CONFIG_LOGLEVEL=$(bashio::config 'loglevel')
 echo "Preparing to run modbus-proxy"
 echo "Upstream: $CONFIG_HOST:$CONFIG_PORT"
 echo "Listen: $CONFIG_LISTENPORT"
+echo "UnitID Remapping: $UNITIDREMAPPING"
 echo "Timeout: $CONFIG_TIMEOUT"
 echo "Connection Time: $CONFIG_CONNECTIONTIME"
 echo "Loglevel: $CONFIG_LOGLEVEL"
@@ -21,6 +23,7 @@ echo "Loglevel: $CONFIG_LOGLEVEL"
 sed -i "s/__HOST__/${CONFIG_HOST}/g" ./modbus.config.yaml
 sed -i "s/__PORT__/${CONFIG_PORT}/g" ./modbus.config.yaml
 sed -i "s/__LISTENPORT__/${CONFIG_LISTENPORT}/g" ./modbus.config.yaml
+sed -i "s/__UNITIDREMAPPING__/${CONFIG_UNITIDREMAPPING}/g" ./modbus.config.yaml
 sed -i "s/__TIMEOUT__/${CONFIG_TIMEOUT}/g" ./modbus.config.yaml
 sed -i "s/__CONNECTIONTIME__/${CONFIG_CONNECTIONTIME}/g" ./modbus.config.yaml
 sed -i "s/__LOGLEVEL__/${CONFIG_LOGLEVEL}/g" ./modbus.config.yaml
